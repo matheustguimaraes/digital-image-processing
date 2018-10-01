26bb3a1ae1da3d31b3fcdf223aade0fb1e77b308
Matheus Guimaraes <matheusguimr@gmail.com>
2018-10-01 14:12:51 -0300

    Add "median filter" function and reformat "mean filter", with their results

--- src/median_filter.py
+++ src/median_filter.py
@@ -3,14 +3,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def get_mean_filter(img, mask_size=3):
+def get_median_filter(img, mask_size=3):
     borders = int(mask_size / 2)
-    mean_filter = img[:, :].copy()
+    median_filter = img[:, :].copy()
     for i in range(borders, img.shape[0] - borders):
         for j in range(borders, img.shape[1] - borders):
-            kernel = img[i - borders:i + borders + 1, j - borders:j + borders + 1]
-            mean_filter[i, j] = int(np.mean(kernel, dtype=np.float32))
-    return mean_filter
+            kernel = np.ravel(img[i - borders:i + borders + 1, j - borders:j + borders + 1])
+            median = sorted(kernel)[int(((mask_size * mask_size) / 2) + 1)]
+            median_filter[i, j] = median
+    return median_filter
 
 
 if __name__ == '__main__':
@@ -21,16 +22,16 @@ if __name__ == '__main__':
     iterator = 3
     images.append(gray_img)
     for k in range(0, 5):
-        images.append(get_mean_filter(gray_img, iterator))
+        images.append(get_median_filter(gray_img, iterator))
         iterator += 2
 
     fig = plt.figure(0)
-    titles = ['Gray Image', '3x3 mask', '5x5 mask', '7x7 mask', '9x9 mask', '11x11 mask']
+    titles = ['Gray image', '3x3 mask', '5x5 mask', '7x7 mask', '9x9 mask', '11x11 mask']
     for x in range(len(images)):
         plt.subplot(2, 3, x + 1), plt.imshow(images[x], 'gray')
         plt.title(titles[x])
-        cv2.imwrite("../results/mean_filter_image_mask_{}.jpg".format(x + 1), images[x])
+        cv2.imwrite("../results/median_filter_image_mask_{}.jpg".format(x + 1), images[x])
 
-    fig.savefig("../results/mean_filter_figure.jpg")
+    fig.savefig("../results/median_filter_figure.jpg")
     plt.show()
     plt.close()
