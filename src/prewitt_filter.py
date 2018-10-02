3ae77dea45f76205da7d853b09ec9e5f31df042a
Matheus Guimaraes <matheusguimr@gmail.com>
2018-10-01 22:05:13 -0300

    Add "prewitt filter", with their results

---
+++ src/prewitt_filter.py
@@ -0,0 +1,56 @@
+import cv2
+import math
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def set_kernel(direction):
+    prewitt_kernel = 0
+    if direction:
+        prewitt_kernel = np.array([[-1, 0, 1],
+                                   [-1, 0, 1],
+                                   [-1, 0, 1]])
+    elif not direction:
+        prewitt_kernel = np.array([[-1, -1, -1],
+                                   [0, 0, 0],
+                                   [1, 1, 1]])
+    return prewitt_kernel
+
+
+def get_prewitt_filter(image):
+    borders = int(3 / 2)
+    prewitt_kernel_x = set_kernel(True)
+    prewitt_kernel_y = set_kernel(False)
+    prewitt_y = np.zeros(image.shape, dtype=np.int16)
+    prewitt_x = np.zeros(image.shape, dtype=np.int16)
+    prewitt_image = np.zeros(image.shape, dtype=np.int16)
+    for i in range(borders, image.shape[0] - borders):
+        for j in range(borders, image.shape[1] - borders):
+            prewitt_y[i][j] = np.sum(image[i - borders:i + borders + 1, j - borders:j + borders + 1] * prewitt_kernel_x)
+            prewitt_x[i][j] = np.sum(image[i - borders:i + borders + 1, j - borders:j + borders + 1] * prewitt_kernel_y)
+            prewitt_image[i][j] = math.sqrt(pow(prewitt_y[i][j], 2) + pow(prewitt_x[i][j], 2))
+    return prewitt_image, prewitt_y, prewitt_x
+
+
+if __name__ == '__main__':
+    img = cv2.imread('../samples/tiger.jpg')
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    images = []
+    images.append(gray_img)
+    prewitt, prewitt_vertical, prewitt_horizontal = get_prewitt_filter(gray_img)
+    images.append(prewitt)
+    images.append(prewitt_vertical)
+    images.append(prewitt_horizontal)
+
+    fig = plt.figure(0)
+    titles = ['Gray Image', 'Total', 'Vertical', 'Horizontal']
+    for x in range(len(images)):
+        plt.subplot(2, 2, x + 1), plt.imshow(images[x], 'gray')
+        plt.title(titles[x])
+        plt.axis('off')
+        cv2.imwrite("../results/prewitt_filter_image_mask_{}.jpg".format(int(x + 1)), images[x])
+
+    fig.savefig("../results/prewitt_filter_figure.jpg")
+    plt.show()
+    plt.close()
