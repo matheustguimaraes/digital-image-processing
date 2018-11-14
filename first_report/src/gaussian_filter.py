af4359379f74953842fa0901a61a65b9d02a0571
Matheus Guimarães <matheusguimr@gmail.com>
2018-11-14 20:34:13 -0300

    Edit gaussian_filter.py to be PEP8 compliant

--- first_report/src/gaussian_filter.py
+++ first_report/src/gaussian_filter.py
@@ -21,13 +21,13 @@ def set_mask_size(mask_size):
     return gaussian_kernel, division
 
 
-def get_gaussian_filter(gray_image, mask_size=3):
-    borders = int(mask_size / 2)
-    gaussian_image = gray_image[:, :].copy()
-    gaussian_kernel, division = set_mask_size(mask_size)
-    for i in range(borders, gray_image.shape[0] - borders):
-        for j in range(borders, gray_image.shape[1] - borders):
-            kernel = gray_image[i - borders:i + borders + 1, j - borders:j + borders + 1] * gaussian_kernel
+def get_gaussian_filter(gray, mask_size=3):
+    bd = int(mask_size / 2)
+    gaussian_image = gray[:, :].copy()
+    gaussian_k, division = set_mask_size(mask_size)
+    for i in range(bd, gray.shape[0] - bd):
+        for j in range(bd, gray.shape[1] - bd):
+            kernel = gray[i - bd:i + bd + 1, j - bd:j + bd + 1] * gaussian_k
             result = np.sum(np.ravel(kernel)) / division
             gaussian_image[i, j] = result
     return gaussian_image
@@ -37,18 +37,18 @@ if __name__ == '__main__':
     img = cv2.imread('../samples/lena.jpg')
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    gaussian_filter_3x3 = get_gaussian_filter(gray_img, 3)
-    gaussian_filter_3x3_2_times = get_gaussian_filter(gaussian_filter_3x3, 3)
-    gaussian_filter_5x5 = get_gaussian_filter(gray_img, 5)
-    gaussian_filter_5x5_2_times = get_gaussian_filter(gaussian_filter_5x5, 5)
-    gaussian_filter_5x5_3_times = get_gaussian_filter(gaussian_filter_5x5_2_times, 5)
+    filter_3x3 = get_gaussian_filter(gray_img, 3)
+    filter_3x3_2_times = get_gaussian_filter(filter_3x3, 3)
+    filter_5x5 = get_gaussian_filter(gray_img, 5)
+    filter_5x5_2_times = get_gaussian_filter(filter_5x5, 5)
+    filter_5x5_3_times = get_gaussian_filter(filter_5x5_2_times, 5)
 
     images = [gray_img,
-              gaussian_filter_3x3,
-              gaussian_filter_3x3_2_times,
-              gaussian_filter_5x5,
-              gaussian_filter_5x5_2_times,
-              gaussian_filter_5x5_3_times]
+              filter_3x3,
+              filter_3x3_2_times,
+              filter_5x5,
+              filter_5x5_2_times,
+              filter_5x5_3_times]
 
     fig = plt.figure(0)
     titles = ['Gray Image',
@@ -57,12 +57,14 @@ if __name__ == '__main__':
               '5x5 mask, 1 time',
               '5x5 mask, 2 times',
               '5x5 mask, 3 times']
+
     for x in range(len(images)):
         plt.subplot(2, 3, x + 1), plt.imshow(images[x], 'gray')
         plt.title(titles[x])
         plt.axis('off')
-        cv2.imwrite("../results/gaussian_filter_image_mask_{}.jpg".format(x + 1), images[x])
+        cv2.imwrite("../results/gaussian/mask_{}.jpg".format(x + 1), images[x])
 
-    fig.savefig("../results/gaussian_filter_figure.jpg")
+    fig.savefig("../results/gaussian/figure.jpg")
     plt.show()
     plt.close()
+
